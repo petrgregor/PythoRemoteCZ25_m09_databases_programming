fd166883234cb7ff83881c105871812f54a581da
Petr Gregor <petr.gregor@email.cz>
2024-11-23 21:29:08 +0100

    MySQL - bikesharing Exercise 1-2.

--- p01_mysql/p09_task_2.py
+++ p01_mysql/p09_task_2.py
@@ -1,6 +1,6 @@
 """Task 2
 Write a script that will create a music database with the table:
-instruments: isntrument_id(PK, autoincrement), name, family, difficulty(enum - easy, medium, hard)
+instruments: instrument_id(PK, autoincrement), name, family, difficulty(enum - easy, medium, hard)
 """
 
 from mysql.connector import connect, Error
